39f25881a71f30dd9d318819dfc3bfdfaccc07d8
sophia1824 <mohaung@gmail.com>
2020-11-14 22:53:08 +0800

    First commit

---
+++ hellopython.py
@@ -0,0 +1,16 @@
+import sys
+import math
+import os
+# l =['123', '456', '789']
+# it=iter(l)
+#
+# while True:
+#     try:
+#         print(next(it))
+#     except StopIteration:
+#         sys.exit()
+
+content=dir(math)
+print(content)
+os.getcwd()
+print("Current working directory", os.getcwd())
